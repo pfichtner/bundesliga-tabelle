c0a05dbe8d7d6537d0c908eacec6a128c63cef47
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 17:10:00 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -43,7 +43,7 @@ class TabelleTest {
 		}
 
 		private Paarung reverse() {
-			return builder().team1(team2).team2(team1).tore(gegentore).gegentore(tore).build();
+			return builder().team1(team2).team2(team1).wayPlayed(wayPlayed).tore(gegentore).gegentore(tore).build();
 		}
 
 		private static class PaarungBuilder {
