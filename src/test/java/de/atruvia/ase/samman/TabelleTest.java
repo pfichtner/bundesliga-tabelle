0add70fd089286fde23d5091ec0b40cb1ba26a12
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 13:58:28 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -1,14 +1,22 @@
 package de.atruvia.ase.samman;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.function.IntPredicate;
 
 import org.junit.jupiter.api.Test;
 
 class TabelleTest {
 
+	private String platz1;
+	private String platz2;
+
 	@Test
 	void zweiMannschaftenKeinSpiel() {
-		fail("Not yet implemented");
+
+		assertThat(platz1).isEqualTo("Team1");
+		assertThat(platz2).isEqualTo("Team2");
 	}
 
 }
