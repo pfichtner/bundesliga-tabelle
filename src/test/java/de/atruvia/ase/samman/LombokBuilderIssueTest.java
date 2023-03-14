422cf416789ffc212cb2939d1aced2aaa772a88d
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 17:27:19 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/LombokBuilderIssueTest.java
+++ src/test/java/de/atruvia/ase/samman/LombokBuilderIssueTest.java
@@ -1,5 +1,16 @@
 package de.atruvia.ase.samman;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class LombokBuilderIssueTest {
+	
+	
+	
+	@Test
+	void testName() {
+		
+	}
 
 }
