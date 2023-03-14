273740f3c14f3886fdef5e99a1e0c7ad002c8ff7
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 17:29:37 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/LombokBuilderIssueTest.java
+++ src/test/java/de/atruvia/ase/samman/LombokBuilderIssueTest.java
@@ -1,5 +1,6 @@
 package de.atruvia.ase.samman;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,8 @@ public class LombokBuilderIssueTest {
 	@Test
 	void testName() {
 		MyPojo myPojo1 = MyPojo.builder().stringValue("string").intValue(42).booleanValue(true).build();
+		MyPojo myPojo2 = myPojo1.toBuilder().build();
+		assertThat(myPojo1).isEqualTo(myPojo2);
 	}
 
 }
