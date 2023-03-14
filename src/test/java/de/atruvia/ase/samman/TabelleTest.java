2242fafa5c496fc944b31b186bee6e2a14e28c68
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 13:55:43 +0100

    autocommit on change

---
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -0,0 +1,14 @@
+package de.atruvia.ase.samman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TabelleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
