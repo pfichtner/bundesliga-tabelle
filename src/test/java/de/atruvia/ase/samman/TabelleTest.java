f7124481d9bbe0ff59c4d6c59f6a4845049f738e
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 17:04:31 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -16,8 +16,7 @@ import lombok.Value;
 
 class TabelleTest {
 
-	private static final String team1 = "Team 1";
-	private static final String team2 = "Team 2";
+	private static final String TEAM_1 = "Team 1";
 
 	@Value
 	@Builder
@@ -64,28 +63,28 @@ class TabelleTest {
 
 	@Test
 	void zweiMannschaftenKeinSpiel() {
-		gegenSeienDiePaarungen(paarung(team1, team2), paarung(team2, team1));
+		gegenSeienDiePaarungen(paarung(TEAM_1, "Team 2"), paarung("Team 2", TEAM_1));
 		wennDieTabelleBerechnetWird();
-		dannIstDieTabelle(tabellenplatz().platz(1).team(team1), tabellenplatz().platz(2).team(team2));
+		dannIstDieTabelle(tabellenplatz().platz(1).team(TEAM_1), tabellenplatz().platz(2).team("Team 2"));
 	}
 
 	@Test
 	void zweiMannschaftenEinSpielKeineTore() {
-		gegenSeienDiePaarungen(paarung(team1, team2).score(0, 0), paarung(team2, team1));
+		gegenSeienDiePaarungen(paarung(TEAM_1, "Team 2").score(0, 0), paarung("Team 2", TEAM_1));
 		wennDieTabelleBerechnetWird();
 		dannIstDieTabelle( //
-				tabellenplatz().platz(1).team(team1).punkte(1), //
-				tabellenplatz().platz(2).team(team2).punkte(1) //
+				tabellenplatz().platz(1).team(TEAM_1).punkte(1), //
+				tabellenplatz().platz(2).team("Team 2").punkte(1) //
 		);
 	}
 
 	@Test
 	void zweiMannschaftenZweiSpieleMitToren() {
-		gegenSeienDiePaarungen(paarung(team1, team2).score(1, 0), paarung(team2, team1).score(1, 0));
+		gegenSeienDiePaarungen(paarung(TEAM_1, "Team 2").score(1, 0), paarung("Team 2", TEAM_1).score(1, 0));
 		wennDieTabelleBerechnetWird();
 		dannIstDieTabelle( //
-				tabellenplatz().platz(1).team(team1).punkte(3).tore(1).gegentore(1), //
-				tabellenplatz().platz(2).team(team2).punkte(3).tore(1).gegentore(1) //
+				tabellenplatz().platz(1).team(TEAM_1).punkte(3).tore(1).gegentore(1), //
+				tabellenplatz().platz(2).team("Team 2").punkte(3).tore(1).gegentore(1) //
 		);
 	}
 
