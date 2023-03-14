57ed2ce0b1971e1456b3a5970ba5aca8864c2ed0
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 17:37:05 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -85,7 +85,7 @@ class TabelleTest {
 		wennDieTabelleBerechnetWird();
 		dannIstDieTabelle( //
 				tabellenplatz().platz(1).team(TEAM_1).punkte(3).tore(1).gegentore(1), //
-				tabellenplatz().platz(2).team(TEAM_2).punkte(3).tore(1).gegentore(1) //
+				tabellenplatz().platz(1).team(TEAM_2).punkte(3).tore(1).gegentore(1) //
 		);
 	}
 
