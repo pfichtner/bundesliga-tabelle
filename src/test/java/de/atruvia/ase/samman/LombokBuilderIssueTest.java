fa97c478f9dcfa95263797ed93b393ef1ae29977
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 17:27:51 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/LombokBuilderIssueTest.java
+++ src/test/java/de/atruvia/ase/samman/LombokBuilderIssueTest.java
@@ -4,9 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import lombok.Builder;
+
 public class LombokBuilderIssueTest {
 	
-	
+	@Builder(toBuilder = true)
+	static class MyPojo {
+		
+	}
 	
 	@Test
 	void testName() {
