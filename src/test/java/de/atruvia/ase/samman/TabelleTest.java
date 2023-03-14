c136a3046077d85d1affac1f4095f0eab1cdf7e7
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-14 16:11:55 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -68,8 +68,16 @@ class TabelleTest {
 	}
 
 	@Test
-	void zweiMannschaftenEinSpiel() {
+	void zweiMannschaftenEinSpielKeineTore() {
 		gegenSeienDiePaarungen(paarung("Team 1", "Team 2").score(0, 0), paarung("Team 2", "Team 1"));
+		wennDieTabelleBerechnetWird();
+		dannIstDieTabelle(tabellenplatz().platz(1).team("Team 1").punkte(1),
+				tabellenplatz().platz(2).team("Team 2").punkte(1));
+	}
+
+	@Test
+	void zweiMannschaftenZweiSpieleMitToren() {
+		gegenSeienDiePaarungen(paarung("Team 1", "Team 2").score(0, 0), paarung("Team 2", "Team 1").score(1, 1));
 		wennDieTabelleBerechnetWird();
 		dannIstDieTabelle(tabellenplatz().platz(1).team("Team 1").punkte(1),
 				tabellenplatz().platz(2).team("Team 2").punkte(1));
