85517ada4573592989724ba5a09e8d6be553a0d9
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-15 17:05:14 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -49,8 +49,7 @@ class TabelleTest {
 
 		@Override
 		public int compareTo(OrdnungsElement o) {
-			Comparator<OrdnungsElement> c2 = Comparator.comparing(e -> e.tabellenPlatz.getPunkte()).thenComparing(Comparator.comparing(e -> e.tabellenPlatz.getTorDifferenz()));
-			Comparator<OrdnungsElement> c3 = c2.thenComparing(Comparator.comparing(e -> e.tabellenPlatz.getTore()));
+			Comparator<OrdnungsElement> c3 = Comparator.comparing(e -> e.tabellenPlatz.getPunkte()).thenComparing(Comparator.comparing(e -> e.tabellenPlatz.getTorDifferenz())).thenComparing(Comparator.comparing(e -> e.tabellenPlatz.getTore()));
 			Comparator<OrdnungsElement> c4 = c3.thenComparing(Comparator.comparing(e -> e.tabellenPlatz.getGegentore()));
 			// TODO Auto-generated method stub
 			return 0;
