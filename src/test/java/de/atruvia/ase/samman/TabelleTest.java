50ec542ced84c6e73dd5e52129796a12ed73fbc9
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-15 17:10:21 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -247,7 +247,8 @@ class TabelleTest {
 					.sorted(comparing(TabellenPlatz::getPunkte, reverseOrder()))
 					.collect(groupingBy(OrdnungsElement::new));
 
-			a.entrySet().stream().sorted(Entry::getKey);
+			Comparator<Entry<OrdnungsElement, List<TabellenPlatz>>> ccc;
+			a.entrySet().stream().sorted(ccc);
 
 			return eintraege.entrySet().stream().map(this::tabellenPlatz)
 					.sorted(comparing(TabellenPlatz::getPunkte, reverseOrder())).collect(toList());
