335df77f8f2372e2079befc912b59309c0661039
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-15 01:22:19 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -46,7 +46,7 @@ class TabelleTest {
 		@Builder.Default
 		int spiele = 1;
 		@Builder.Default
-		Map<Ergebnis, Integer> ergebnisse;
+		Map<Ergebnis, Integer> ergebnisse = new ;
 		int punkte;
 		int tore;
 		int gegentore;
