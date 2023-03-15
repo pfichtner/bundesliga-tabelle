a4e4b326dc390da0b05205b39264205568b059f1
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-15 17:05:33 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -49,7 +49,7 @@ class TabelleTest {
 
 		@Override
 		public int compareTo(OrdnungsElement o) {
-			Comparator<OrdnungsElement> c4 = Comparator.comparing(e -> e.tabellenPlatz.getPunkte())
+			Comparator<OrdnungsElement> c4 = Comparator.<OrdnungsElement>comparing(e -> e.tabellenPlatz.getPunkte())
 					.thenComparing(Comparator.comparing(e -> e.tabellenPlatz.getTorDifferenz()))
 					.thenComparing(Comparator.comparing(e -> e.tabellenPlatz.getTore()))
 					.thenComparing(Comparator.comparing(e -> e.tabellenPlatz.getGegentore()));
