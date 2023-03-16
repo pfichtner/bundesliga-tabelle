dfd5f0c8de26d8ba1b56d11c288952add83e71a3
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:21:27 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
+++ src/main/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
@@ -1,7 +1,15 @@
 package de.atruvia.ase.sammanbuli.infra.adapters.secondary;
 
+import java.util.List;
+
+import de.atruvia.ase.sammanbuli.domain.Paarung;
 import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
 
 public class OpenLigaDbSpieltagRepo implements SpieltagRepo {
+
+	public List<Paarung> lade(String string, String string2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
---
+++ src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepoTest.java
@@ -0,0 +1,17 @@
+package de.atruvia.ase.sammanbuli.infra.adapters.secondary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OpenLigaDbSpieltagRepoTest {
+
+	@Test
+	void canReadFirstMatch() {
+		
+		OpenLigaDbSpieltagRepo repo = new OpenLigaDbSpieltagRepo();
+		
+		fail("Not yet implemented");
+	}
+
+}
