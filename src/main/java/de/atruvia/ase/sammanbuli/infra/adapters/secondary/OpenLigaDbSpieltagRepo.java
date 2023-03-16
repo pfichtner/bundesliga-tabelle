6a5d9b35547150b5f393e0a413920db0ccfa1c5d
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:21:52 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
+++ src/main/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
@@ -7,6 +7,8 @@ import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
 
 public class OpenLigaDbSpieltagRepo implements SpieltagRepo {
 
+	String url = "https://api.openligadb.de/getmatchdata/bl1/2022";
+
 	public List<Paarung> lade(String league, String season) {
 		// TODO Auto-generated method stub
 		return null;
--- src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepoTest.java
+++ src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepoTest.java
@@ -16,7 +16,6 @@ class OpenLigaDbSpieltagRepoTest {
 	@Test
 	void canRetrieveDataOf2022() throws IOException, InterruptedException, URISyntaxException {
 		OpenLigaDbSpieltagRepo repo = new OpenLigaDbSpieltagRepo();
-		String url = "https://api.openligadb.de/getmatchdata/bl1/2022";
 		List<Paarung> paarungen = repo.lade("bl1", "2022");
 		assertThat(paarungen.get(0)).isEqualTo(
 				Paarung.builder().team1("Eintracht Frankfurt").team2("FC Bayern München").ergebnis(1, 6).build());
