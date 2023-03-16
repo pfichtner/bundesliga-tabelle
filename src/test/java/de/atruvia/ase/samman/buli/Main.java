55091ec42b83727d78aecc5586b4376d286cedaa
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:41:17 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/Main.java
+++ src/test/java/de/atruvia/ase/samman/buli/Main.java
@@ -9,16 +9,13 @@ import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
 import de.atruvia.ase.sammanbuli.infra.adapters.secondary.OpenLigaDbSpieltagRepo;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		SpieltagRepo repo = new OpenLigaDbSpieltagRepo();
 		TabellenService tabellenService = new DefaultTabellenService(repo);
 		for (TabellenPlatz tabellenPlatz : tabellenService.erstelleTabelle("bl1", "2022")) {
 			System.out.println(tabellenPlatz);
 		}
-
-		
-		
 	}
 
 }
