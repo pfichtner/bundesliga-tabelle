21123432ddac22caed9aaf0e6c100dfaa27fddab
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:22:22 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
+++ src/main/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
@@ -1,7 +1,18 @@
 package de.atruvia.ase.sammanbuli.infra.adapters.secondary;
 
+import static java.nio.charset.Charset.defaultCharset;
+import static java.util.stream.Collectors.toList;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.List;
 
+import com.google.gson.Gson;
+
+import de.atruvia.ase.samman.buli.infra.DataRetrieveTest.Match;
 import de.atruvia.ase.sammanbuli.domain.Paarung;
 import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
 
@@ -10,8 +21,9 @@ public class OpenLigaDbSpieltagRepo implements SpieltagRepo {
 	String url = "https://api.openligadb.de/getmatchdata/bl1/2022";
 
 	public List<Paarung> lade(String league, String season) {
-		// TODO Auto-generated method stub
-		return null;
+		String content = Files.contentOf(new File(getClass().getClassLoader().getResource("2022.json").toURI()),
+				defaultCharset());
+		return Arrays.stream(new Gson().fromJson(content, Match[].class)).map(Match::toDomain).collect(toList());
 	}
 
 }
