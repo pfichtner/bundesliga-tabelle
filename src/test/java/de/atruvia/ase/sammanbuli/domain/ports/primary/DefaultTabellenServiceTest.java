7acc8af36424f3914349f6d8cba0a9f2002ebec5
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 17:42:08 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/sammanbuli/domain/ports/primary/DefaultTabellenServiceTest.java
+++ src/test/java/de/atruvia/ase/sammanbuli/domain/ports/primary/DefaultTabellenServiceTest.java
@@ -3,15 +3,20 @@ package de.atruvia.ase.sammanbuli.domain.ports.primary;
 import static de.atruvia.ase.sammanbuli.infra.adapters.secondary.OpenLigaDbSpieltagRepoMother.readFromLocalFilesystemRepo;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
+import de.atruvia.ase.sammanbuli.domain.TabellenPlatz;
 import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
 
 class DefaultTabellenServiceTest {
 
 	@Test
 	void test() {
-		DefaultTabellenService sut = new DefaultTabellenService(readFromLocalFilesystemRepo());
+		TabellenService sut = new DefaultTabellenService(readFromLocalFilesystemRepo());
+		List<TabellenPlatz> erstelleTabelle = sut.erstelleTabelle("bl1", "2022");
+		
 		fail("Not yet implemented");
 	}
 
