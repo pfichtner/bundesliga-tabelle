842a619a1a6e581a641dc9d4cc5f9b2357825b64
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:23:41 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
+++ src/main/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
@@ -44,7 +44,8 @@ public class OpenLigaDbSpieltagRepo implements SpieltagRepo {
 	}
 
 	public List<Paarung> lade(String league, String season) {
-		String content = Files.contentOf(new File(getClass().getClassLoader().getResource("2022.json").toURI()),
+		File file = new File(getClass().getClassLoader().getResource("2022.json").toURI());
+		String content = Files.contentOf(file,
 				defaultCharset());
 		return Arrays.stream(new Gson().fromJson(content, Match[].class)).map(Match::toDomain).collect(toList());
 	}
