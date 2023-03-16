b47f5fa7f05edae262e0d20f246d406af2d30ce6
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:35:05 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/sammanbuli/domain/TabelleTest.java
+++ src/test/java/de/atruvia/ase/sammanbuli/domain/TabelleTest.java
@@ -5,9 +5,7 @@ import static de.atruvia.ase.sammanbuli.domain.Paarung.Ergebnis.SIEG;
 import static de.atruvia.ase.sammanbuli.domain.Paarung.Ergebnis.UNENTSCHIEDEN;
 import static java.util.Collections.emptyMap;
 import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
@@ -15,14 +13,12 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
 
 import de.atruvia.ase.sammanbuli.domain.Paarung.Ergebnis;
-import de.atruvia.ase.sammanbuli.domain.TabelleTest.TabellenPlatz.TabellenPlatzBuilder;
+import de.atruvia.ase.sammanbuli.domain.TabelleTest.TabellenPlatz;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -206,55 +202,6 @@ class TabelleTest {
 
 	private void gegenSeienDiePaarungen(Paarung.PaarungBuilder... paarungen) {
 		this.paarungen = Arrays.stream(paarungen).map(Paarung.PaarungBuilder::build).toArray(Paarung[]::new);
-	}
-
-	private static class Tabelle {
-
-		private final Map<String, TabellenPlatz> eintraege = new HashMap<>();
-
-		private void add(Paarung paarung) {
-			addInternal(paarung, false);
-			addInternal(paarung.swap(), true);
-		}
-
-		private void addInternal(Paarung paarung, boolean swapped) {
-			eintraege.merge(paarung.getTeam1(), newEintrag(paarung, swapped), TabellenPlatz::merge);
-		}
-
-		private TabellenPlatz newEintrag(Paarung paarung, boolean swapped) {
-			if (!paarung.isGespielt()) {
-				return TabellenPlatz.NULL;
-			}
-			TabellenPlatzBuilder builder = TabellenPlatz.builder() //
-					.ergebnis(paarung.ergebnis()) //
-					.punkte(paarung.punkte());
-			if (swapped) {
-				return builder //
-						.toreAuswaerts(paarung.getTore()) //
-						.gegentoreAuswaerts(paarung.getGegentore()) //
-						.build();
-			} else
-				return builder //
-						.toreHeim(paarung.getTore()) //
-						.gegentoreHeim(paarung.getGegentore()) //
-						.build();
-		}
-
-		public List<TabellenPlatz> getEntries() {
-			// TODO make it side-affect-free, does it work W/O zip!?
-			AtomicInteger platz = new AtomicInteger();
-			Map<OrdnungsElement, List<TabellenPlatz>> platzGruppen = eintraege.entrySet().stream().map(this::setTeam)
-					.collect(groupingBy(OrdnungsElement::new));
-			return platzGruppen.entrySet().stream().sorted(comparing(Entry::getKey)).peek(e -> platz.incrementAndGet())
-					.map(Entry::getValue).flatMap(t -> t.stream().sorted(comparing(OrdnungsElement::new))
-							.map(tp -> tp.withPlatz(platz.get())))
-					.collect(toList());
-		}
-
-		private TabellenPlatz setTeam(Entry<String, TabellenPlatz> entry) {
-			return entry.getValue().withTeam(entry.getKey());
-		}
-
 	}
 
 	private void wennDieTabelleBerechnetWird() {
