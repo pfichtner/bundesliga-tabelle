fe5b220347547d9332d0b2ed9f74f00949ae5b4d
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 20:46:18 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepoMother.java
+++ src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepoMother.java
@@ -4,6 +4,8 @@ import static java.nio.file.Files.readString;
 import static lombok.AccessLevel.PRIVATE;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
 
 import lombok.NoArgsConstructor;
 
@@ -15,8 +17,12 @@ public final class OpenLigaDbSpieltagRepoMother {
 			@Override
 			protected String readJson(String league, String season) throws Exception {
 				return readString(
-						new File(getClass().getClassLoader().getResource(league + "/" + season + ".json").toURI())
-								.toPath());
+						extracted(league, season));
+			}
+
+			private Path extracted(String league, String season) throws URISyntaxException {
+				return new File(getClass().getClassLoader().getResource(league + "/" + season + ".json").toURI())
+						.toPath();
 			}
 		};
 	}
