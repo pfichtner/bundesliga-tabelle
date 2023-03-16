d673a679395e4a8b1dc2e48e3f12fd484636155e
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 13:26:06 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/TabelleTest.java
@@ -263,9 +263,9 @@ class TabelleTest {
 		public List<TabellenPlatz> getEntries() {
 			// TODO make it side-affect-free, does it work W/O zip!?
 			AtomicInteger platz = new AtomicInteger();
-			Map<OrdnungsElement, List<TabellenPlatz>> collect = eintraege.entrySet().stream().map(this::setTeam)
+			Map<OrdnungsElement, List<TabellenPlatz>> gruppiertMitPlatz = eintraege.entrySet().stream().map(this::setTeam)
 					.collect(groupingBy(OrdnungsElement::new));
-			return collect.entrySet().stream().sorted(comparing(Entry::getKey, reverseOrder()))
+			return gruppiertMitPlatz.entrySet().stream().sorted(comparing(Entry::getKey, reverseOrder()))
 					.peek(e -> platz.incrementAndGet()).map(Entry::getValue).flatMap(t -> t.stream()
 							.sorted(comparing(OrdnungsElement::new)).map(tp -> tp.withPlatz(platz.get())))
 					.collect(toList());
