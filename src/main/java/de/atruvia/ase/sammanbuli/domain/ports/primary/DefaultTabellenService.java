065655bf7810a0017d1567647262cc440605de0f
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:39:10 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/sammanbuli/domain/ports/primary/DefaultTabellenService.java
+++ src/main/java/de/atruvia/ase/sammanbuli/domain/ports/primary/DefaultTabellenService.java
@@ -1,5 +1,8 @@
 package de.atruvia.ase.sammanbuli.domain.ports.primary;
 
+import java.util.List;
+
+import de.atruvia.ase.sammanbuli.domain.Paarung;
 import de.atruvia.ase.sammanbuli.domain.Tabelle;
 import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +13,12 @@ public class DefaultTabellenService implements TabellenService {
 	private final SpieltagRepo spieltagRepo;
 
 	@Override
-	public void erstelleTabelle(String league, String season) {
+	public void erstelleTabelle(String league, String season) throws Exception {
 		Tabelle tabelle = new Tabelle();
-		
-		spieltagRepo.l
-		
-		tabelle.add(p);
-		
-		
+
+		List<Paarung> lade = spieltagRepo.lade(league, season);
+		lade.forEach(tabelle::add);
+
 		// TODO Auto-generated method stub
 
 	}
