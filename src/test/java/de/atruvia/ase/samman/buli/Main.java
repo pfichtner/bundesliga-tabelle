50770beadc2c2c6bfc6b3f6fd8537bfcf2688ca3
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:12:24 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/Main.java
+++ src/test/java/de/atruvia/ase/samman/buli/Main.java
@@ -1,6 +1,6 @@
 package de.atruvia.ase.samman.buli;
 
-import de.atruvia.ase.sammanbuli.domain.TabellenService;
+import de.atruvia.ase.sammanbuli.domain.ports.primary.TabellenService;
 import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
 
 public class Main {
