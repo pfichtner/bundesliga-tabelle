aaae9bdec5071a5828173a5a9d884a0304f5ef08
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:16:12 +0100

    autocommit on change

---
+++ src/main/java/de/atruvia/ase/sammanbuli/domain/Paarung.java
@@ -0,0 +1,52 @@
+package de.atruvia.ase.sammanbuli.domain;
+
+import static de.atruvia.ase.sammanbuli.domain.TabelleTest.Ergebnis.NIEDERLAGE;
+import static de.atruvia.ase.sammanbuli.domain.TabelleTest.Ergebnis.SIEG;
+import static de.atruvia.ase.sammanbuli.domain.TabelleTest.Ergebnis.UNENTSCHIEDEN;
+
+import de.atruvia.ase.sammanbuli.domain.TabelleTest.Ergebnis;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
+public class Paarung {
+
+	boolean gespielt;
+	String team1, team2;
+	int tore;
+	int gegentore;
+
+	public int punkte() {
+		return switch (ergebnis()) {
+		case SIEG -> 3;
+		case UNENTSCHIEDEN -> 1;
+		case NIEDERLAGE -> 0;
+		};
+	}
+
+	public Ergebnis ergebnis() {
+		return tore == gegentore ? UNENTSCHIEDEN : tore > gegentore ? SIEG : NIEDERLAGE;
+	}
+
+	Paarung swap() {
+		return toBuilder().team1(team2).team2(team1).tore(gegentore).gegentore(tore).build();
+	}
+
+	public static class PaarungBuilder {
+
+		public Paarung.PaarungBuilder ergebnis(int tore, int gegentore) {
+			this.gespielt = true;
+			this.tore = tore;
+			this.gegentore = gegentore;
+			return this;
+		}
+
+		private Paarung.PaarungBuilder gespielt(boolean gespielt) {
+			this.gespielt = gespielt;
+			return this;
+		}
+
+	}
+
+}
