1a529d276023c006f6abcaba5f2c10ac0ef93b4a
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 14:59:37 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/infra/DataRetrieveIT.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/DataRetrieveIT.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
+import de.atruvia.ase.samman.Paarung;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
