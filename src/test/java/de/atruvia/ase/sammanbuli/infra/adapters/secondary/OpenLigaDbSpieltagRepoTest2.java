5ca3f64f3012a0198438c081117853f3ff642009
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:32:46 +0100

    autocommit on change

---
+++ src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepoTest2.java
@@ -0,0 +1,33 @@
+package de.atruvia.ase.sammanbuli.infra.adapters.secondary;
+
+import static java.nio.file.Files.readString;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import de.atruvia.ase.sammanbuli.domain.Paarung;
+
+class OpenLigaDbSpieltagRepoTest2 {
+
+	@Test
+	void canRetrieveDataOf2022() throws Exception {
+		List<Paarung> paarungen = repo().lade("bl1", "2022");
+		assertThat(paarungen.get(0)).isEqualTo(
+				Paarung.builder().team1("Eintracht Frankfurt").team2("FC Bayern München").ergebnis(1, 6).build());
+	}
+
+	private OpenLigaDbSpieltagRepo repo() {
+		return new OpenLigaDbSpieltagRepo() {
+			@Override
+			protected String readJson(String league, String season) throws Exception {
+				return readString(
+						new File(getClass().getClassLoader().getResource(league + "/" + season + ".json").toURI())
+								.toPath());
+			}
+		};
+	}
+
+}
