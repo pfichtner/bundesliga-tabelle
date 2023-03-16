077f08da61d4730d28d2ac4cfa1f455a1e3508f5
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 14:12:52 +0100

    autocommit on change

---
+++ src/test/java/de/atruvia/ase/samman/Paarung.java
@@ -0,0 +1,45 @@
+package de.atruvia.ase.samman;
+
+import static de.atruvia.ase.samman.TabelleTest.Ergebnis.NIEDERLAGE;
+import static de.atruvia.ase.samman.TabelleTest.Ergebnis.SIEG;
+import static de.atruvia.ase.samman.TabelleTest.Ergebnis.UNENTSCHIEDEN;
+
+import de.atruvia.ase.samman.TabelleTest.Ergebnis;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true) class Paarung {
+	boolean gespielt;
+	String team1, team2;
+	int tore;
+	int gegentore;
+
+	int punkte() {
+		return switch (ergebnis()) {
+		case SIEG -> 3;
+		case UNENTSCHIEDEN -> 1;
+		case NIEDERLAGE -> 0;
+		};
+	}
+
+	public Ergebnis ergebnis() {
+		return tore == gegentore ? UNENTSCHIEDEN : tore > gegentore ? SIEG : NIEDERLAGE;
+	}
+
+	Paarung swap() {
+		return toBuilder().team1(team2).team2(team1).tore(gegentore).gegentore(tore).build();
+	}
+
+	static class PaarungBuilder {
+
+		public Paarung.PaarungBuilder ergebnis(int tore, int gegentore) {
+			this.gespielt = true;
+			this.tore = tore;
+			this.gegentore = gegentore;
+			return this;
+		}
+
+	}
+
+}
