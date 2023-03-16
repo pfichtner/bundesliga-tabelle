cfe28e453c937b99689b93b398b9ee60ed00f32a
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:38:28 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/sammanbuli/domain/ports/secondary/SpieltagRepo.java
+++ src/main/java/de/atruvia/ase/sammanbuli/domain/ports/secondary/SpieltagRepo.java
@@ -1,5 +1,11 @@
 package de.atruvia.ase.sammanbuli.domain.ports.secondary;
 
+import java.util.List;
+
+import de.atruvia.ase.sammanbuli.domain.Paarung;
+
 public interface SpieltagRepo {
+
+	List<Paarung> lade(String league, String season) throws Exception;
 
 }
