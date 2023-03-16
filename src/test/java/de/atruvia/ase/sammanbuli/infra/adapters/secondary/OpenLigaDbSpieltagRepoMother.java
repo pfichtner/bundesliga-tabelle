da6abc7c598929c574558cb8061e5e1bffbdc2f7
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 17:40:22 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepoMother.java
+++ src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/secondary/OpenLigaDbSpieltagRepoMother.java
@@ -1,8 +1,21 @@
 package de.atruvia.ase.sammanbuli.infra.adapters.secondary;
 
+import static java.nio.file.Files.readString;
+
+import java.io.File;
+
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class OpenLigaDbSpieltagRepoMother {
+public class OpenLigaDbSpieltagRepoMother {public static OpenLigaDbSpieltagRepo readFromLocalFilesystemRepo() {
+		return new OpenLigaDbSpieltagRepo() {
+			@Override
+			protected String readJson(String league, String season) throws Exception {
+				return readString(
+						new File(getClass().getClassLoader().getResource(league + "/" + season + ".json").toURI())
+								.toPath());
+			}
+		};
+	}
 
 }
