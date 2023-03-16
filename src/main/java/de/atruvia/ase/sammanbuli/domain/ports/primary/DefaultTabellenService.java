0ec17ef4030afdaf97e5a212dcfeaa23d0f03b06
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-16 15:40:29 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/sammanbuli/domain/ports/primary/DefaultTabellenService.java
+++ src/main/java/de/atruvia/ase/sammanbuli/domain/ports/primary/DefaultTabellenService.java
@@ -2,7 +2,6 @@ package de.atruvia.ase.sammanbuli.domain.ports.primary;
 
 import java.util.List;
 
-import de.atruvia.ase.sammanbuli.domain.Paarung;
 import de.atruvia.ase.sammanbuli.domain.Tabelle;
 import de.atruvia.ase.sammanbuli.domain.TabellenPlatz;
 import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
