9c98963e25e8e074f70591ab4c13f4ce6f3324ff
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 18:46:37 +0100

    autocommit on change

---
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbWappenRepoRepoTest.java
@@ -0,0 +1,21 @@
+package de.atruvia.ase.samman.buli.infra.adapters.secondary;
+
+import static de.atruvia.ase.samman.buli.infra.adapters.secondary.OpenLigaDbSpieltagRepoMother.readFromLocalFilesystemRepo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import de.atruvia.ase.samman.buli.domain.Paarung;
+
+class OpenLigaDbWappenRepoRepoTest {
+
+	@Test
+	void canRetrieveDataOf2022() throws Exception {
+		List<Paarung> paarungen = readFromLocalFilesystemRepo().lade("bl1", "2022");
+		assertThat(paarungen).hasSize(306).element(0).isEqualTo(
+				Paarung.builder().team1("Eintracht Frankfurt").team2("FC Bayern München").ergebnis(1, 6).build());
+	}
+
+}
