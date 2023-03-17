b9e9db49e8779423e96d9c6b18361615572a25c8
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 18:54:58 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbWappenRepo.java
+++ src/main/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbWappenRepo.java
@@ -1,23 +1,15 @@
 package de.atruvia.ase.samman.buli.infra.adapters.secondary;
 
-import static java.util.stream.Collectors.toList;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
 
 import org.springframework.stereotype.Repository;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 
-import de.atruvia.ase.samman.buli.domain.Paarung;
-import de.atruvia.ase.samman.buli.domain.Paarung.PaarungBuilder;
-import de.atruvia.ase.samman.buli.domain.ports.secondary.SpieltagRepo;
 import de.atruvia.ase.samman.buli.domain.ports.secondary.WappenRepository;
 import lombok.ToString;
 
@@ -39,12 +31,6 @@ class OpenLigaDbWappenRepo implements WappenRepository {
 	public URI getWappen(String league, String season, String teamName) throws Exception {
 		return Arrays.stream(new Gson().fromJson(readJson(league, season), TeamInfo[].class))
 				.filter(t -> t.teamName.equals(teamName)).findFirst().map(TeamInfo::toDomain).orElse(null);
-	}
-
-	@Override
-	public List<URI> lade(String league, String season) throws Exception {
-		return Arrays.stream(new Gson().fromJson(readJson(league, season), TeamInfo[].class)).map(TeamInfo::toDomain)
-				.collect(toList());
 	}
 
 	protected String readJson(String league, String season) throws Exception {
