75ad3a050156b8bbc8d8bd83324adf8df67e7860
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 17:04:36 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/primary/HttpAdapterTest.java
+++ src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/primary/HttpAdapterTest.java
@@ -38,11 +38,11 @@ class HttpAdapterTest {
 
 	@Test
 	void shouldReturnDefaultMessage() throws Exception {
-		TabellenPlatz p1 = TabellenPlatz.builder().team("Team 10").platz(11).spiele(12).punkte(13).toreHeim(14)
+		TabellenPlatz platz1 = TabellenPlatz.builder().team("Team 10").platz(11).spiele(12).punkte(13).toreHeim(14)
 				.toreAuswaerts(15).build();
-		TabellenPlatz p2 = TabellenPlatz.builder().team("Team 20").platz(21).spiele(22).punkte(23).toreHeim(24)
+		TabellenPlatz platz2 = TabellenPlatz.builder().team("Team 20").platz(21).spiele(22).punkte(23).toreHeim(24)
 				.toreAuswaerts(25).build();
-		Mockito.when(tabellenService.erstelleTabelle("bl1", "2022")).thenReturn(List.of(p1, p2));
+		Mockito.when(tabellenService.erstelleTabelle("bl1", "2022")).thenReturn(List.of(platz1, platz2));
 
 		this.mockMvc.perform(get("/tabelle/bl1/2022")) //
 				.andDo(print()) //
