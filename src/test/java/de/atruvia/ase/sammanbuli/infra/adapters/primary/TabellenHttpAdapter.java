5c287277bd45e58a656538bb706c027b8b2c6df2
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 16:41:32 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/primary/TabellenHttpAdapter.java
+++ src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/primary/TabellenHttpAdapter.java
@@ -1,5 +1,15 @@
 package de.atruvia.ase.sammanbuli.infra.adapters.primary;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TabellenHttpAdapter {
+
+	@GetMapping("/tabelle/{league}/{season}")
+	public String getTabelle(@PathVariable String league, @PathVariable String season) {
+		return "Hello, World";
+	}
 
 }
