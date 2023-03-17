155e84cfb15f01ea9e9e5e93245d59d8caaa9f4c
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 19:56:21 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/domain/TabelleTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/domain/TabelleTest.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
@@ -65,7 +66,7 @@ class TabelleTest {
 		gegenSeienDiePaarungen(paarung("Team 1", "Team 2", URI.create("wappenAlt1"), URI.create("wappenAlt2")),
 				paarung("Team 2", "Team 1", URI.create("wappenNeu1"), URI.create("wappenNeu2")));
 		wennDieTabelleBerechnetWird();
-		assertThat(sut.getEntries().stream().map(t -> t.getWappen() = null ? "": t.getWappen().toASCIIString()).collect(joining("\n")))
+		assertThat(sut.getEntries().stream().map(t -> Optional.ofNullable(t).map(TabbellenPlatz::getWappen).collect(joining("\n")))
 				.isEqualTo("""
 						a
 						b""");
