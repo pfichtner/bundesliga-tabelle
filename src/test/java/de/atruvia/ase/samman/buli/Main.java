8540b983352188fc1ca8054e19923cf1cdc28288
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 16:41:59 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/Main.java
+++ src/test/java/de/atruvia/ase/samman/buli/Main.java
@@ -1,9 +1,12 @@
 package de.atruvia.ase.samman.buli;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import de.atruvia.ase.sammanbuli.domain.ports.primary.DefaultTabellenService;
 import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
 import de.atruvia.ase.sammanbuli.infra.adapters.secondary.OpenLigaDbSpieltagRepo;
 
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
