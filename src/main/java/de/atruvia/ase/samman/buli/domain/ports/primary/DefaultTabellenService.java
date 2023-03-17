8b8234870918d0ec00376887b1aaf77e9d44167a
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 21:01:59 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenService.java
+++ src/main/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenService.java
@@ -1,11 +1,6 @@
 package de.atruvia.ase.samman.buli.domain.ports.primary;
 
-import static java.util.stream.Collectors.toList;
-
-import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -14,40 +9,14 @@ import de.atruvia.ase.samman.buli.domain.Tabelle;
 import de.atruvia.ase.samman.buli.domain.TabellenPlatz;
 import de.atruvia.ase.samman.buli.domain.ports.secondary.SpieltagRepo;
 import de.atruvia.ase.samman.buli.domain.ports.secondary.TeamRepo;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 class DefaultTabellenService implements TabellenService {
 
 	private final SpieltagRepo spieltagRepo;
 	private final TeamRepo wappenRepo;
-
-	public DefaultTabellenService(SpieltagRepo spieltagRepo, TeamRepo wappenRepo) {
-		this.spieltagRepo = spieltagRepo;
-		this.wappenRepo = new TeamRepo() {
-
-			private final Map<String, URI> cache = new HashMap<>();
-
-			@Override
-			public URI getTeams(String league, String season, String team) throws Exception {
-				return getTeams(league, season);
-			}
-
-			@Override
-			public URI getTeams(String league, String season) throws Exception {
-				return cache.computeIfAbsent(team, t -> load(wappenRepo, league, season, team));
-			}
-
-			private URI load(TeamRepo wappenRepo, String league, String season, String team) {
-				try {
-					return wappenRepo.getTeams(league, season);
-				} catch (Exception e) {
-					// TODO Log error
-					e.printStackTrace();
-					return null;
-				}
-			}
-		};
-	}
 
 	@Override
 	public List<TabellenPlatz> erstelleTabelle(String league, String season) {
@@ -55,8 +24,6 @@ class DefaultTabellenService implements TabellenService {
 		lade(league, season).forEach(tabelle::add);
 		return tabelle.getEntries();
 	}
-
-	
 
 	private List<Paarung> lade(String league, String season) {
 		try {
