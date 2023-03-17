2b0414a6655b99baadf53874ca2750055a7078c1
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 21:34:08 +0100

    autocommit on change

---
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbWappenRepoIT.java
@@ -0,0 +1,23 @@
+package de.atruvia.ase.samman.buli.infra.adapters.secondary;
+
+import static de.atruvia.ase.samman.buli.infra.adapters.secondary.OpenLigaDbSpieltagRepoMother.teamFsRepo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+
+class OpenLigaDbWappenRepoIT {
+
+	@Test
+	void canRetrieveDataOf2022() throws Exception {
+		assertTeamHasWappen("Eintracht Frankfurt", "https://i.imgur.com/X8NFkOb.png");
+		assertTeamHasWappen("FC Bayern München", "https://i.imgur.com/jJEsJrj.png");
+	}
+
+	void assertTeamHasWappen(String teamName, String wappenUri) throws Exception {
+		assertThat(teamFsRepo().getTeams("bl1", "2022").stream().filter(t -> t.getName().equals(teamName)).findFirst())
+				.hasValueSatisfying(t -> assertThat(t.getWappen()).isEqualTo(URI.create(wappenUri)));
+	}
+
+}
