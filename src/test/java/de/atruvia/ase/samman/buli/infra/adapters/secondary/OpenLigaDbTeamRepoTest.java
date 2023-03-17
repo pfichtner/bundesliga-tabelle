67476227eb653b6b031ed7ed5306f7f550bf9547
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 21:35:41 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbTeamRepoTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbTeamRepoTest.java
@@ -16,8 +16,12 @@ class OpenLigaDbTeamRepoTest {
 	}
 
 	void assertTeamHasWappen(String teamName, String wappenUri) throws Exception {
-		assertThat(teamFsRepo().getTeams("bl1", "2022").stream().filter(t -> t.getName().equals(teamName)).findFirst())
+		assertThat(repo().getTeams("bl1", "2022").stream().filter(t -> t.getName().equals(teamName)).findFirst())
 				.hasValueSatisfying(t -> assertThat(t.getWappen()).isEqualTo(URI.create(wappenUri)));
+	}
+
+	private OpenLigaDbTeamRepo repo() {
+		return teamFsRepo();
 	}
 
 }
