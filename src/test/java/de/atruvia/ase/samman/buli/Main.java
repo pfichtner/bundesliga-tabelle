9dff0897ebed6b2d0d45873a936c4c479244d52a
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 17:14:59 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/Main.java
+++ src/test/java/de/atruvia/ase/samman/buli/Main.java
@@ -1,20 +1,13 @@
 package de.atruvia.ase.samman.buli;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import de.atruvia.ase.sammanbuli.domain.ports.primary.DefaultTabellenService;
-import de.atruvia.ase.sammanbuli.domain.ports.secondary.SpieltagRepo;
-import de.atruvia.ase.sammanbuli.infra.adapters.secondary.OpenLigaDbSpieltagRepo;
 
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) {
-		new DefaultTabellenService(spieltagRepo()).erstelleTabelle("bl1", "2022").forEach(System.out::println);
-	}
-
-	private static SpieltagRepo spieltagRepo() {
-		return new OpenLigaDbSpieltagRepo();
+	public static void main(String... args) {
+		SpringApplication.run(Main.class, args);
 	}
 
 }
