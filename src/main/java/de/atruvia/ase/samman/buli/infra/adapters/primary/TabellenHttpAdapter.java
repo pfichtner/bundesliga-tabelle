0ef2276bb283b441513b47a9504d29ca29091526
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 17:31:53 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/samman/buli/infra/adapters/primary/TabellenHttpAdapter.java
+++ src/main/java/de/atruvia/ase/samman/buli/infra/adapters/primary/TabellenHttpAdapter.java
@@ -1,6 +1,6 @@
 package de.atruvia.ase.samman.buli.infra.adapters.primary;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,17 +8,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 import de.atruvia.ase.samman.buli.domain.TabellenPlatz;
 import de.atruvia.ase.samman.buli.domain.ports.primary.TabellenService;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
 @RestController
 @RequiredArgsConstructor
 public class TabellenHttpAdapter {
-	
+
+	@Value
+	@Builder
+	private static class JsonTabellenPlatz {
+
+		int platz;
+		String team;
+		int spiele = 1;
+		int punkte;
+		int tore;
+		int gegentore;
+
+		int siege;
+		int unentschienden;
+		int niederlagen;
+
+		public static JsonTabellenPlatz fromDomain(TabellenPlatz t) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
 	private final TabellenService tabellenService;
 
 	@GetMapping("/tabelle/{league}/{season}")
-	public List<TabellenPlatz> getTabelle(@PathVariable String league, @PathVariable String season) {
-		return tabellenService.erstelleTabelle(league, season);
+	public Stream<JsonTabellenPlatz> getTabelle(@PathVariable String league, @PathVariable String season) {
+		return tabellenService.erstelleTabelle(league, season).stream().map(JsonTabellenPlatz::fromDomain);
 	}
 
 }
