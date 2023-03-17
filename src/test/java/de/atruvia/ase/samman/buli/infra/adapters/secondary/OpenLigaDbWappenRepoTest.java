d5126f468c677409a82103c989a4194cb7126ae6
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 19:03:55 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbWappenRepoTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbWappenRepoTest.java
@@ -1,21 +1,18 @@
 package de.atruvia.ase.samman.buli.infra.adapters.secondary;
 
-import static de.atruvia.ase.samman.buli.infra.adapters.secondary.OpenLigaDbSpieltagRepoMother.spieltagFsRepo;
+import static de.atruvia.ase.samman.buli.infra.adapters.secondary.OpenLigaDbSpieltagRepoMother.wappenFsRepo;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
+import java.net.URI;
 
 import org.junit.jupiter.api.Test;
-
-import de.atruvia.ase.samman.buli.domain.Paarung;
 
 class OpenLigaDbWappenRepoTest {
 
 	@Test
 	void canRetrieveDataOf2022() throws Exception {
-		List<Paarung> paarungen = spieltagFsRepo().lade("bl1", "2022");
-		assertThat(paarungen).hasSize(306).element(0).isEqualTo(
-				Paarung.builder().team1("Eintracht Frankfurt").team2("FC Bayern München").ergebnis(1, 6).build());
+		assertThat(wappenFsRepo().getWappen("bl1", "2022", "Eintracht Frankfurt")).isEqualTo(URI.create(""));
+		assertThat(wappenFsRepo().getWappen("bl1", "2022", "FC Bayern München")).isEqualTo(URI.create(""));
 	}
 
 }
