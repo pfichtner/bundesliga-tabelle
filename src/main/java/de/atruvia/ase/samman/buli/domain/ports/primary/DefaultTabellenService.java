aea35f50b8f87f97f6bfde5db392b56ef7ac0083
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 19:08:48 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenService.java
+++ src/main/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenService.java
@@ -8,6 +8,7 @@ import de.atruvia.ase.samman.buli.domain.Paarung;
 import de.atruvia.ase.samman.buli.domain.Tabelle;
 import de.atruvia.ase.samman.buli.domain.TabellenPlatz;
 import de.atruvia.ase.samman.buli.domain.ports.secondary.SpieltagRepo;
+import de.atruvia.ase.samman.buli.domain.ports.secondary.WappenRepo;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -15,10 +16,11 @@ import lombok.RequiredArgsConstructor;
 class DefaultTabellenService implements TabellenService {
 
 	private final SpieltagRepo spieltagRepo;
+	private final WappenRepo wappenRepo;
 
 	@Override
 	public List<TabellenPlatz> erstelleTabelle(String league, String season) {
-		Tabelle tabelle = new Tabelle((league1, season1, team) -> null);
+		Tabelle tabelle = new Tabelle(wappenRepo);
 		lade(league, season).forEach(tabelle::add);
 		return tabelle.getEntries();
 	}
