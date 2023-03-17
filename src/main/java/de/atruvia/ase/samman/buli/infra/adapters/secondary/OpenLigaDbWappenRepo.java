df2e66bca244b3c335892b40c0d3f8ab72f0a2f6
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 20:58:09 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbWappenRepo.java
+++ src/main/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbWappenRepo.java
@@ -6,12 +6,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
 import com.google.gson.Gson;
 
-import de.atruvia.ase.samman.buli.domain.Team;
 import de.atruvia.ase.samman.buli.domain.ports.secondary.TeamRepo;
 import lombok.ToString;
 
@@ -32,7 +32,7 @@ class OpenLigaDbWappenRepo implements TeamRepo {
 	@Override
 	public List<Team> getTeams(String league, String season) throws Exception {
 		return Arrays.stream(new Gson().fromJson(readJson(league, season), Team[].class)).map(Team::toDomain)
-				.orElse(null);
+				.collect(Collectors.toList())
 	}
 
 	protected String readJson(String league, String season) throws Exception {
