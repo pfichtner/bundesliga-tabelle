7d24b72970930e5ee3dcc0dec56f2f01aad05975
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 19:09:21 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenServiceTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenServiceTest.java
@@ -1,6 +1,6 @@
 package de.atruvia.ase.samman.buli.domain.ports.primary;
 
-import static de.atruvia.ase.samman.buli.infra.adapters.secondary.OpenLigaDbSpieltagRepoMother.spieltagFsRepo;
+import static de.atruvia.ase.samman.buli.infra.adapters.secondary.OpenLigaDbSpieltagRepoMother.*;
 import static java.util.stream.Collectors.joining;
 import static org.approvaltests.Approvals.verify;
 
@@ -16,7 +16,7 @@ class DefaultTabellenServiceTest {
 
 	@Test
 	void tabelleBl12022Spieltag24() {
-		TabellenService sut = new DefaultTabellenService(spieltagFsRepo());
+		TabellenService sut = new DefaultTabellenService(spieltagFsRepo(), wappenFsRepo());
 		List<TabellenPlatz> erstelleTabelle = sut.erstelleTabelle("bl1", "2022");
 		String tabelle = erstelleTabelle.stream().map(f -> print(f, longestTeamName(erstelleTabelle)))
 				.collect(joining("\n"));
