c091ac3697962ff848ac98b676979013405685e8
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 17:11:15 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/primary/HttpAdapterTest.java
+++ src/test/java/de/atruvia/ase/sammanbuli/infra/adapters/primary/HttpAdapterTest.java
@@ -1,5 +1,8 @@
 package de.atruvia.ase.sammanbuli.infra.adapters.primary;
 
+import static de.atruvia.ase.sammanbuli.domain.Paarung.Ergebnis.NIEDERLAGE;
+import static de.atruvia.ase.sammanbuli.domain.Paarung.Ergebnis.SIEG;
+import static de.atruvia.ase.sammanbuli.domain.Paarung.Ergebnis.UNENTSCHIEDEN;
 import static java.util.stream.Collectors.joining;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.containsString;
@@ -23,7 +26,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import de.atruvia.ase.samman.buli.Main;
-import de.atruvia.ase.sammanbuli.domain.Paarung.Ergebnis;
 import de.atruvia.ase.sammanbuli.domain.TabellenPlatz;
 import de.atruvia.ase.sammanbuli.domain.ports.primary.DefaultTabellenService;
 import de.atruvia.ase.sammanbuli.domain.ports.primary.TabellenService;
@@ -43,8 +45,11 @@ class HttpAdapterTest {
 	@Test
 	void shouldReturnDefaultMessage() throws Exception {
 		TabellenPlatz platz1 = TabellenPlatz.builder().team("Team 10").spiele(11)
-				.ergebnisse(Map.of(Ergebnis.SIEG, 12, Ergebnis.UNENTSCHIEDEN, 13, Ergebnis.NIEDERLAGE, 14)).toreHeim(15)
-				.toreAuswaerts(16).gegentoreHeim(17).gegentoreAuswaerts(18).punkte(19).build();
+				.ergebnisse(Map.of(SIEG, 12, UNENTSCHIEDEN, 13, NIEDERLAGE, 14)).toreHeim(15).toreAuswaerts(16)
+				.gegentoreHeim(17).gegentoreAuswaerts(18).punkte(19).build();
+		TabellenPlatz platz2 = TabellenPlatz.builder().team("Team 20").spiele(21)
+				.ergebnisse(Map.of(SIEG, 22, UNENTSCHIEDEN, 23, NIEDERLAGE, 24)).toreHeim(25).toreAuswaerts(26)
+				.gegentoreHeim(27).gegentoreAuswaerts(28).punkte(29).build();
 		when(tabellenService.erstelleTabelle("bl1", "2022")).thenReturn(List.of(platz1, platz2));
 
 		this.mockMvc.perform(get("/tabelle/bl1/2022")) //
