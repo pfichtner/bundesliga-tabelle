607510b2283002ec4b2c8b45eb7ae91cf5f086bb
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-17 19:29:58 +0100

    autocommit on change

--- src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepoTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepoTest.java
@@ -16,7 +16,7 @@ class OpenLigaDbSpieltagRepoTest {
 	void canRetrieveDataOf2022() throws Exception {
 		List<Paarung> paarungen = spieltagFsRepo().lade("bl1", "2022");
 		assertThat(paarungen).hasSize(306).element(0).isEqualTo(Paarung.builder().team1("Eintracht Frankfurt")
-				.team2("FC Bayern München").ergebnis(1, 6).wappen1(URI.create("")).wappen2(URI.create("")).build());
+				.team2("FC Bayern München").ergebnis(1, 6).wappen1(URI.create("https://i.imgur.com/X8NFkOb.png")).wappen2(URI.create("")).build());
 	}
 
 }
