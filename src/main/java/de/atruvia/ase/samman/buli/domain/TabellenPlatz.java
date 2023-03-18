0e4118e8e341315c5e6bdc03e635dbcdd630267d
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-18 13:52:50 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/samman/buli/domain/TabellenPlatz.java
+++ src/main/java/de/atruvia/ase/samman/buli/domain/TabellenPlatz.java
@@ -3,6 +3,7 @@ package de.atruvia.ase.samman.buli.domain;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.NIEDERLAGE;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.SIEG;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.UNENTSCHIEDEN;
+import static java.lang.Math.max;
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 
@@ -96,7 +97,7 @@ public class TabellenPlatz {
 	}
 
 	public List<Ergebnis> getLetzte(int count) {
-		List<Ergebnis> copy = new ArrayList<>(ergebnisse.subList(ergebnisse.size() - 5, ergebnisse.size()));
+		List<Ergebnis> copy = new ArrayList<>(ergebnisse.subList(max(0, ergebnisse.size() - 5), ergebnisse.size()));
 		Collections.reverse(copy);
 		return copy;
 	}
