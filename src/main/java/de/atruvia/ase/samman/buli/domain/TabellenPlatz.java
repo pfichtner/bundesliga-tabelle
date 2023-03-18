037f57336388f4989aeb0faadb23a77130195f99
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-03-18 12:36:59 +0100

    autocommit on change

--- src/main/java/de/atruvia/ase/samman/buli/domain/TabellenPlatz.java
+++ src/main/java/de/atruvia/ase/samman/buli/domain/TabellenPlatz.java
@@ -3,12 +3,16 @@ package de.atruvia.ase.samman.buli.domain;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.NIEDERLAGE;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.SIEG;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.UNENTSCHIEDEN;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Stream;
@@ -22,7 +26,7 @@ import lombok.With;
 @Builder
 public class TabellenPlatz {
 
-	static TabellenPlatz NULL = new TabellenPlatz(null, 0, "", 0, emptyMap(), 0, 0, 0, 0, 0);
+	static TabellenPlatz NULL = new TabellenPlatz(null, 0, "", 0, emptyMap(), emptyList(), 0, 0, 0, 0, 0);
 
 	@With
 	URI wappen;
@@ -33,6 +37,7 @@ public class TabellenPlatz {
 	@Builder.Default
 	int spiele = 1;
 	Map<Ergebnis, Integer> ergebnisseOld;
+	List<Ergebnis> ergebnisse;
 	int punkte;
 	int toreHeim;
 	int toreAuswaerts;
@@ -50,10 +55,12 @@ public class TabellenPlatz {
 	public static class TabellenPlatzBuilder {
 
 		public TabellenPlatzBuilder() {
+			ergebnisse = new ArrayList<>();
 			ergebnisseOld = new HashMap<>();
 		}
 
 		public TabellenPlatzBuilder ergebnis(Ergebnis ergebnis) {
+			ergebnisse.add(ergebnis);
 			ergebnisseOld.merge(ergebnis, 1, (a, b) -> a + b);
 			return this;
 		}
@@ -66,6 +73,7 @@ public class TabellenPlatz {
 
 	public TabellenPlatz merge(TabellenPlatz other) {
 		return builder() //
+				.ergebnisse(merge(this.ergebnisse, other.ergebnisse))
 				.ergebnisseOld(merge(this.ergebnisseOld, other.ergebnisseOld)) //
 				.spiele(this.spiele + other.spiele) //
 				.punkte(this.punkte + other.punkte) //
@@ -75,6 +83,10 @@ public class TabellenPlatz {
 				.gegentoreAuswaerts(this.gegentoreAuswaerts + other.gegentoreAuswaerts) //
 				.wappen(other.wappen == null ? this.wappen : other.wappen) //
 				.build();
+	}
+
+	private List<Ergebnis> merge(List<Ergebnis> ergebnisse1, List<Ergebnis> ergebnisse2) {
+		return Stream.concat(ergebnisse1.stream(), ergebnisse2.stream()).collect(toList());
 	}
 
 	private static Map<Ergebnis, Integer> merge(Map<Ergebnis, Integer> map1, Map<Ergebnis, Integer> map2) {
