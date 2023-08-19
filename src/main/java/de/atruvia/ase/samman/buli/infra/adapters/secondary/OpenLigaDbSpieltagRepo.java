343395ae28353e906854c64710a41975931a3f3f
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-08-19 12:42:33 +0200

    matchresult order changed and unfolded bug in identifying final match result

--- src/main/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
+++ src/main/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepo.java
@@ -1,11 +1,13 @@
 package de.atruvia.ase.samman.buli.infra.adapters.secondary;
 
+import static java.util.Arrays.stream;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
@@ -27,6 +29,13 @@ class OpenLigaDbSpieltagRepo implements SpieltagRepo {
 
 	@ToString
 	private class MatchResult {
+		static final int RESULTTYPEID_ENDERGEBIS = 2;
+
+		boolean isEndergebnis() {
+			return resultTypeID == RESULTTYPEID_ENDERGEBIS;
+		}
+
+		int resultTypeID;
 		int pointsTeam1;
 		int pointsTeam2;
 	}
@@ -40,15 +49,23 @@ class OpenLigaDbSpieltagRepo implements SpieltagRepo {
 		Paarung toDomain() {
 			PaarungBuilder b = Paarung.builder().team1(team1.teamName).team2(team2.teamName);
 			b = b.wappen1(URI.create(team1.teamIconUrl)).wappen2(URI.create(team2.teamIconUrl));
-			b = matchResults.length == 0 ? b : b.ergebnis(matchResults[0].pointsTeam1, matchResults[0].pointsTeam2);
+			if (matchResults.length > 0) {
+				MatchResult finalResult = stream(matchResults).filter(MatchResult::isEndergebnis).findFirst()
+						.orElseThrow(() -> new IllegalStateException("No final result found"));
+				b = b.ergebnis(finalResult.pointsTeam1, finalResult.pointsTeam2);
+			}
 			return b.build();
 		}
+
+		private static Optional<MatchResult> endergebnis(MatchResult[] matchResults) {
+			return stream(matchResults).filter(MatchResult::isEndergebnis).findFirst();
+		}
+
 	}
 
 	@Override
 	public List<Paarung> lade(String league, String season) throws Exception {
-		return Arrays.stream(new Gson().fromJson(readJson(league, season), Match[].class)).map(Match::toDomain)
-				.toList();
+		return stream(new Gson().fromJson(readJson(league, season), Match[].class)).map(Match::toDomain).toList();
 	}
 
 	protected String readJson(String league, String season) throws Exception {
--- src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepoTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepoTest.java
@@ -17,11 +17,32 @@ class OpenLigaDbSpieltagRepoTest {
 		List<Paarung> paarungen = repo().lade("bl1", "2022");
 		assertThat(paarungen).hasSize(306).element(0)
 				.isEqualTo(Paarung.builder().team1("Eintracht Frankfurt").team2("FC Bayern München").ergebnis(1, 6)
-						.wappen1(URI.create("https://i.imgur.com/X8NFkOb.png"))
-						.wappen2(URI.create("https://i.imgur.com/jJEsJrj.png")).build());
+						.wappen1(wappenFrankfurt()).wappen2(wappenMuenchen()).build());
 	}
 
-	private OpenLigaDbSpieltagRepo repo() {
+	@Test
+	void canRetrieveDataOf2023() throws Exception {
+		// 2022: "endergebnis" was first element of array, 2023 it was last -> filter
+		// "resultTypeID" = 2 for now
+		List<Paarung> paarungen = repo().lade("bl1", "2023");
+		assertThat(paarungen).hasSize(9).element(0).isEqualTo(Paarung.builder().team1("Werder Bremen")
+				.team2("FC Bayern München").ergebnis(0, 4).wappen1(wappenBremen()).wappen2(wappenMuenchen()).build());
+	}
+
+	URI wappenFrankfurt() {
+		return URI.create("https://i.imgur.com/X8NFkOb.png");
+	}
+
+	URI wappenMuenchen() {
+		return URI.create("https://i.imgur.com/jJEsJrj.png");
+	}
+
+	URI wappenBremen() {
+		return URI.create(
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/SV-Werder-Bremen-Logo.svg/681px-SV-Werder-Bremen-Logo.svg.png");
+	}
+
+	OpenLigaDbSpieltagRepo repo() {
 		return spieltagFsRepo();
 	}
 
