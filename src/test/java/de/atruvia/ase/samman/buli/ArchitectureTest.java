a82bff77011d2200f46a3578ac60dafd3181127a
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-08-28 13:12:28 +0200

    introducing archunit

---
+++ src/test/java/de/atruvia/ase/samman/buli/ArchitectureTest.java
@@ -0,0 +1,32 @@
+package de.atruvia.ase.samman.buli;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packages = "de.atruvia.ase.samman.buli", importOptions = DoNotIncludeTests.class)
+class ArchitectureTest {
+
+	@ArchTest
+	ArchRule portsAndAdapters = onionArchitecture() //
+			.withOptionalLayers(true) //
+			.domainModels("..domain..") //
+			.adapter("primary-adapters", "..adapters.primary..") //
+			.adapter("secondary-adapters", "..adapters.secondary..") //
+	;
+
+	@ArchTest
+	ArchRule noCoreToAdaptersDependencies = noClasses().that().resideInAPackage("..domain..") //
+			.should().dependOnClassesThat().resideInAPackage("..adapters..") //
+	;
+
+	@ArchTest
+	ArchRule noPortsToAdaptersDependencies = noClasses().that().resideInAPackage("..ports..") //
+			.should().dependOnClassesThat().resideInAPackage("..adapters..") //
+	;
+
+}
