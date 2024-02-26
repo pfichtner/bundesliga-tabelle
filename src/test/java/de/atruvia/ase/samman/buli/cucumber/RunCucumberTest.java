2836f6cdebbd1674955ef5dce14aaa3e3901fd33
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-02-26 21:33:10 +0100

    cucumber via junit-platform-suite

---
+++ src/test/java/de/atruvia/ase/samman/buli/cucumber/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package de.atruvia.ase.samman.buli.cucumber;
+
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("de/atruvia/ase/samman/buli/domain")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "de.atruvia.ase.samman.buli.cucumber")
+public class RunCucumberTest {
+}
