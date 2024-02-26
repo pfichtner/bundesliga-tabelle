940544e46a83cb50dbcd11d1f07ba42f7f7f3e82
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-02-26 17:12:41 +0100

    cucumber added

---
+++ src/test/java/de/atruvia/ase/samman/buli/cucumber/StepDefs.java
@@ -0,0 +1,58 @@
+package de.atruvia.ase.samman.buli.cucumber;
+
+import static java.lang.Integer.parseInt;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.assertj.core.api.SoftAssertions;
+
+import de.atruvia.ase.samman.buli.domain.Paarung;
+import de.atruvia.ase.samman.buli.domain.Paarung.PaarungBuilder;
+import de.atruvia.ase.samman.buli.domain.Tabelle;
+import de.atruvia.ase.samman.buli.domain.TabellenPlatz;
+import io.cucumber.java.de.Dann;
+import io.cucumber.java.de.Gegebensei;
+import io.cucumber.java.de.Wenn;
+
+public class StepDefs {
+
+	List<Paarung> paarungen = new ArrayList<>();
+	Tabelle tabelle = new Tabelle();
+	List<TabellenPlatz> entries;
+
+	@Gegebensei("ein Spielplan")
+	public void ein_spielplan(io.cucumber.datatable.DataTable dataTable) {
+		for (Map<String, String> row : dataTable.asMaps()) {
+			String[] ergebnis = row.get("Ergebnis").split(":");
+			paarungen.add(PaarungBuilder.paarung(row.get("Heim"), row.get("Gast"))
+					.ergebnis(parseInt(ergebnis[0]), parseInt(ergebnis[1])).build());
+		}
+	}
+
+	@Wenn("die Tabelle berechnet wird")
+	public void die_tabelle_berechnet_wird() {
+		for (Paarung paarung : paarungen) {
+			tabelle.add(paarung);
+		}
+		entries = tabelle.getEntries();
+	}
+
+	@Dann("ist die Tabelle")
+	public void ist_die_tabelle(io.cucumber.datatable.DataTable dataTable) {
+		Iterator<TabellenPlatz> iterator = entries.iterator();
+		for (Map<String, String> row : dataTable.asMaps()) {
+			TabellenPlatz tabellenPlatz = iterator.next();
+			assertSoftly(s -> {
+				s.assertThat(tabellenPlatz.getTeam()).isEqualTo(row.get("Team"));
+				s.assertThat(tabellenPlatz.getPlatz()).isEqualTo(parseInt(row.get("Platz")));
+				// TODO more to assert
+			});
+		}
+	}
+
+}
---
+++ src/test/resources/de/atruvia/ase/samman/buli/domain/Tabelle.feature
@@ -0,0 +1,18 @@
+# language: de
+
+Funktionalität: Tabelle erstellen
+
+Szenario: Zwei Mannschaften sind punkt- und torgleich
+
+	Gegeben sei ein Spielplan
+		| Heim   | Gast   | Ergebnis |
+		| Team 1 | Team 2 |      1:0 |
+		| Team 2 | Team 1 |      1:0 |
+		| Team 1 | Team 3 |      1:0 |
+		| Team 2 | Team 3 |      1:0 |
+	Wenn die Tabelle berechnet wird
+	Dann ist die Tabelle
+		| Platz|Team  |Spiele|Siege|Unentschieden|Niederlagen|Punkte|Tore|Gegentore|Tordifferenz
+		| 1    |Team 1|3     |    2|            0|          1|     6|   2|        1|           1
+		| 1    |Team 2|3     |    2|            0|          1|     6|   2|        1|           1
+		| 3    |Team 3|2     |    0|            0|          2|     0|   0|        2|          -2
