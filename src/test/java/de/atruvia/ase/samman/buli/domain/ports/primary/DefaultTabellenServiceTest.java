14079ed64d578ce8de0e3e18704f28f34ee7fea8
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-04-26 13:53:08 +0200

    removed approval tests that have unit tests

--- src/test/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenServiceTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenServiceTest.java
@@ -1,46 +1,10 @@
 package de.atruvia.ase.samman.buli.domain.ports.primary;
 
-import static de.atruvia.ase.samman.buli.infra.adapters.secondary.OpenLigaDbSpieltagRepoMother.spieltagFsRepo;
-import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.joining;
-import static org.approvaltests.Approvals.verify;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-
-import java.util.List;
-import java.util.Objects;
 
 import org.junit.jupiter.api.Test;
 
-import de.atruvia.ase.samman.buli.domain.TabellenPlatz;
-
 class DefaultTabellenServiceTest {
-
-	@Test
-	void tabelleBl12022Spieltag24() {
-		TabellenService sut = new DefaultTabellenService(spieltagFsRepo());
-		List<TabellenPlatz> erstellteTabelle = sut.erstelleTabelle("bl1", "2022");
-		String tabelle = erstellteTabelle.stream().map(f -> print(f, longestTeamName(erstellteTabelle)))
-				.collect(joining("\n"));
-		verify(tabelle);
-	}
-
-	@Test
-	void tabelleBl12023Spieltag27_gamesRunning_goalsButFinalResultsAre_0_0() {
-		TabellenService sut = new DefaultTabellenService(spieltagFsRepo());
-		List<TabellenPlatz> erstellteTabelle = sut.erstelleTabelle("bl1", "2023-games-running");
-		String tabelle = erstellteTabelle.stream().map(f -> print(f, longestTeamName(erstellteTabelle)))
-				.collect(joining("\n"));
-		verify(tabelle);
-	}
-
-	@Test
-	void tabelleBl12023Spieltag27_gamesRunning_goalsAndFinalResultsAreCorrect() {
-		TabellenService sut = new DefaultTabellenService(spieltagFsRepo());
-		List<TabellenPlatz> erstellteTabelle = sut.erstelleTabelle("bl1", "2023-games-running-correct-final-result");
-		String tabelle = erstellteTabelle.stream().map(f -> print(f, longestTeamName(erstellteTabelle)))
-				.collect(joining("\n"));
-		verify(tabelle);
-	}
 
 	@Test
 	void whenRepoThrowsExceptionThenTheServiceThrowsTheException() {
@@ -50,33 +14,6 @@ class DefaultTabellenServiceTest {
 		});
 		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> sut.erstelleTabelle("bl1", "2022"))
 				.withFailMessage(message);
-	}
-
-	int longestTeamName(List<TabellenPlatz> tabellenPlaetze) {
-		return tabellenPlaetze.stream().map(TabellenPlatz::team).mapToInt(String::length).max().orElse(0);
-	}
-
-	String print(TabellenPlatz tabellenPlatz, int length) {
-		return asList( //
-				stringFormat(length, tabellenPlatz.team()), //
-				tabellenPlatz.spiele(), //
-				tabellenPlatz.siege(), //
-				tabellenPlatz.unentschieden(), //
-				tabellenPlatz.niederlagen(), //
-				tabellenPlatz.tore(), //
-				tabellenPlatz.gegentore(), //
-				tabellenPlatz.torDifferenz(), //
-				tabellenPlatz.punkte(), //
-				tabellenPlatz.wappen() //
-		).stream().map(this::format).collect(joining(" | "));
-	}
-
-	String stringFormat(int length, String team) {
-		return String.format("%-" + (length + 1) + "s", team);
-	}
-
-	String format(Object o) {
-		return o instanceof Number ? "%3d".formatted(o) : Objects.toString(o);
 	}
 
 }
