34266c4f42c2721b637bd123175895b244db406c
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-04-27 10:44:47 +0200

    supporting provider-state 'team #1 is currently playing'

--- src/test/java/de/atruvia/ase/samman/buli/infra/adapters/primary/ContractVerificationTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/primary/ContractVerificationTest.java
@@ -3,10 +3,15 @@ package de.atruvia.ase.samman.buli.infra.adapters.primary;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.NIEDERLAGE;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.SIEG;
 import static de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis.UNENTSCHIEDEN;
+import static de.atruvia.ase.samman.buli.domain.Paarung.PaarungBuilder.paarung;
 import static de.atruvia.ase.samman.buli.domain.PaarungMother.paarungen;
+import static java.lang.Integer.MAX_VALUE;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
@@ -22,6 +27,8 @@ import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import de.atruvia.ase.samman.buli.Main;
+import de.atruvia.ase.samman.buli.domain.Paarung;
+import de.atruvia.ase.samman.buli.domain.Paarung.PaarungBuilder;
 import de.atruvia.ase.samman.buli.domain.ports.secondary.SpieltagRepo;
 
 @Provider("BundesligaBackend")
@@ -51,6 +58,17 @@ class ContractVerificationTest {
 	void matchdayThreeWinDrawLoss() throws Exception {
 		when(spieltagRepoMock.lade(anyString(), anyString()))
 				.thenReturn(paarungen("anyTeamName", SIEG, UNENTSCHIEDEN, NIEDERLAGE));
+	}
+
+	@State("team #1 is currently playing")
+	void runningGame() throws Exception {
+		var paarung1 = paarung("Team 1", "Team 3").zwischenergebnis(0, MAX_VALUE);
+		var paarung2 = paarung("Team 2", "Team 4").endergebnis(0, 0);
+		when(spieltagRepoMock.lade(anyString(), anyString())).thenReturn(build(paarung1, paarung2));
+	}
+
+	private static List<Paarung> build(PaarungBuilder... paarungen) {
+		return Stream.of(paarungen).map(PaarungBuilder::build).toList();
 	}
 
 }
