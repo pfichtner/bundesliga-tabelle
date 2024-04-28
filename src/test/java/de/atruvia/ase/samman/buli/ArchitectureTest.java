138cd130017e48b30e13efceb729ca03547d2c8e
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-04-28 12:00:10 +0200

    noCycles added

--- src/test/java/de/atruvia/ase/samman/buli/ArchitectureTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/ArchitectureTest.java
@@ -2,14 +2,22 @@ package de.atruvia.ase.samman.buli;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-@AnalyzeClasses(packages = "de.atruvia.ase.samman.buli", importOptions = DoNotIncludeTests.class)
+@AnalyzeClasses(packages = ArchitectureTest.BASE, importOptions = DoNotIncludeTests.class)
 class ArchitectureTest {
+
+	static final String BASE = "de.atruvia.ase.samman.buli";
+
+	@ArchTest
+	ArchRule noCycles = slices().matching(BASE + ".(*)..") //
+			.should().beFreeOfCycles() //
+	;
 
 	@ArchTest
 	ArchRule portsAndAdapters = onionArchitecture() //
