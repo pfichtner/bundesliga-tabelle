84122083ed80c7e228e9c14e0c2e76722dc1ced4
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-05-17 16:07:11 +0200

    containsTheLastNelements added

--- src/test/java/de/atruvia/ase/samman/buli/domain/TendenzTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/domain/TendenzTest.java
@@ -1,8 +1,13 @@
 package de.atruvia.ase.samman.buli.domain;
 
+import static java.lang.Math.max;
+import static java.util.Collections.reverse;
+import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import de.atruvia.ase.samman.buli.domain.Paarung.Ergebnis;
 import de.atruvia.ase.samman.buli.domain.TabellenPlatz.Tendenz;
@@ -14,16 +19,34 @@ class TendenzTest {
 
 	@Property
 	void asciiStringAlwaysAsLongAsLength(@ForAll List<Ergebnis> ergebnisse,
-			@ForAll @IntRange(min = 0, max = 512) int length) {
+			@ForAll @IntRange(min = 0, max = 34) int length) {
 		var value = Tendenz.from(ergebnisse, length).toASCIIString();
 		assertThat(value.chars()).hasSize(length);
 	}
 
 	@Property
 	void asciiStringOnlyContainsSUNorDash(@ForAll List<Ergebnis> ergebnisse,
-			@ForAll @IntRange(min = 0, max = 512) int length) {
+			@ForAll @IntRange(min = 0, max = 34) int length) {
 		var value = Tendenz.from(ergebnisse, length).toASCIIString();
 		assertThat(value.chars()).allMatch(c -> "SUN-".indexOf(c) != -1);
+	}
+
+	@Property
+	void containsTheLastNelements(@ForAll List<Ergebnis> ergebnisse, @ForAll @IntRange(min = 0, max = 34) int length) {
+		String expected = subList(ergebnisse, length).stream().map(e -> e == null ? "-" : String.valueOf(e.charValue()))
+				.collect(joining());
+		var value = Tendenz.from(ergebnisse, length).toASCIIString();
+		assertThat(value).isEqualTo(expected);
+	}
+
+	private List<Ergebnis> subList(List<Ergebnis> ergebnisse, int length) {
+		List<Ergebnis> list = new ArrayList<>(
+				ergebnisse.subList(max(0, ergebnisse.size() - length), ergebnisse.size()));
+		reverse(list);
+		while (list.size() < length) {
+			list.add(null);
+		}
+		return list;
 	}
 
 }
